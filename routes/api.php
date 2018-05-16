d2174aa7fe7d44f8a3b4733e24c7ec7f13f5e2f4
Danielle Emygdio <emygdio.d@gmail.com>
2018-05-16 10:34:04 -0300

    Change DB reference to AWS one and modify routes

--- routes/api.php
+++ routes/api.php
@@ -18,23 +18,36 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('users', function (Request $r) {
-    $users = \DB::table('users')->get();
+    $users = \DB::table('User')->get();
     return $users;
 });
 
 Route::get('isUserCitizen', function (Request $r) {
-    $users = \DB::table('users')->where('type', '=' ,'Citizen')->get();
+    $users = \DB::table('User')
+                  ->where('UserType', '=' ,'Citizen')
+                  ->get();
+
     return $users;
 });
 
 Route::get('services/categories/{cityhallid}', function ($cityhallid) {
-    $categories = \DB::table('cityhallservice')
-                        ->where('cityhall_id', '=', $cityhallid)
-                        ->get();
+    $categories = \DB::select("SELECT Service.ServiceName, Service.ServiceID, Category.CategoryName, Category.CategoryID
+                              FROM CityHall
+                              INNER JOIN CityHallService
+                              	ON CityHall.CityHallID = CityHallService.CityHallID
+                              INNER JOIN Service
+                              	ON CityHallService.ServiceID = Service.ServiceID
+                              INNER JOIN Category
+                              	ON Category.CategoryID = Service.CategoryID
+                              WHERE CityHall.CityHallID =".$cityhallid);
+
     return $categories;
 });
 
-Route::get('services/categories', function (Request $r) {
-    $categories = \DB::table('categories')->get();
-    return $categories;
+Route::get('cityhall/isCity/{cityname}', function ($cityname) {
+    $city = \DB::table('CityHall')
+                        ->select('CityHall.CityName')
+                        ->where('CityHall.CityName', 'LIKE', $cityname)
+                        ->get();
+    return $city;
 });
